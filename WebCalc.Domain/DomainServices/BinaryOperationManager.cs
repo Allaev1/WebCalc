dac14f336076cfe594363ecee6c1593bcbc6485e
Bekzod Allaev <Allaev_@outlook.com>
2022-06-19 19:46:34 +0500

    Implement GetBinaryOperation and GetChainedBinaryOperation methods

--- WebCalc.Domain/DomainServices/BinaryOperationManager.cs
+++ WebCalc.Domain/DomainServices/BinaryOperationManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebCalc.Domain.Entities;
 
 namespace WebCalc.Domain.DomainServices
 {
     public class BinaryOperationManager
     {
+        public BinaryOperation GetBinaryOperation() => new BinaryOperation();
+
+        public BinaryOperation GetChainedBinaryOperation(BinaryOperation operation)
+        {
+            var result = new BinaryOperation();
+
+            operation.CalculateResult();
+            result.FirstOperand = operation.Result;
+
+            return operation;
+        }
     }
 }
