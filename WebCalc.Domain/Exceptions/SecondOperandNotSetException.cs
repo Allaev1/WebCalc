43e07e1c618c803113890e35a5df8ead2eb9f9e0
Bekzod Allaev <Allaev_@outlook.com>
2022-06-19 19:04:55 +0500

    Add SecondOperandNotSetException

---
+++ WebCalc.Domain/Exceptions/SecondOperandNotSetException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Exceptions
+{
+    public class SecondOperandNotSetException : Exception
+    {
+        public SecondOperandNotSetException(string message)
+            : base(message) { }
+    }
+}
