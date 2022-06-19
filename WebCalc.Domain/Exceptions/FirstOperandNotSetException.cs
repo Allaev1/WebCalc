1d7aa1d91b6b7938b7a619ce82b8192f02f3dcc2
Bekzod Allaev <Allaev_@outlook.com>
2022-06-19 18:48:53 +0500

    Add FirstOperandNotSetException

---
+++ WebCalc.Domain/Exceptions/FirstOperandNotSetException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Exceptions
+{
+    public class FirstOperandNotSetException : Exception
+    {
+        public FirstOperandNotSetException(string message)
+            : base(message) { }
+    }
+}
