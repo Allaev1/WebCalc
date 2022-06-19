513bf884f42cf79bd9217632910e8ba78fe337ff
Bekzod Allaev <Allaev_@outlook.com>
2022-06-19 18:27:26 +0500

    Add enumeration of operators

---
+++ WebCalc.Domain/Enums/Operators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Enums
+{
+    public enum Operators
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+}
