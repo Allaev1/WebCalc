0233ba0df9e8d7211507f11761a2598eaf066b6a
Bekzod Allaev <Allaev_@outlook.com>
2022-06-19 19:34:38 +0500

    Add BinaryOperationManager

---
+++ WebCalc.Domain/DomainServices/BinaryOperationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.DomainServices
+{
+    public class BinaryOperationManager
+    {
+    }
+}
