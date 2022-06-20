250074b95807a3132b9f334feb064d93078f63ec
Bekzod Allaev <Allaev_@outlook.com>
2022-06-20 23:00:15 +0500

    Add class for exception message constants

---
+++ WebCalc.Domain/Constants/ExceptionMessageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Constants
+{
+    public static class ExceptionMessageConstants
+    {
+        public const string SET_FIRST_OPERAND_MESSAGE = "Please set first operand";
+        public const string SET_SECOND_OPERAND_MESSAGE = "Please set second operand";
+        public const string SET_OPERATOR_MESSAGE = "Please set operator first";
+    }
+}
