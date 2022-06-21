b110baa125c1b49ec8b9352450d3100a86981504
Bekzod Allaev <Allaev_@outlook.com>
2022-06-21 16:39:44 +0500

    Add Operand class

---
+++ WebCalc.Domain/Entities/Operand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Entities
+{
+    public class Operand
+    {
+        public double Value { get; private set; }
+
+        public int Precision { get; private set; }
+
+        internal Operand() { }
+
+        public void SetValue(string stringValue)
+        {
+            if ((stringValue.Length > 1 && stringValue.First() == '0') || stringValue.Where(x => x == ',').Count() > 1)
+                throw new Exception();
+            Value = double.Parse(stringValue);
+        }
+
+        public void SetPercision(int precision)
+        {
+            Precision = precision;
+
+            Value = Math.Round(Value, precision);
+        }
+    }
+}
