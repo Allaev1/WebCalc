64d161c7ccd41f76c3fbccd9ba7b52dd90a3950f
Bekzod Allaev <Allaev_@outlook.com>
2022-06-21 17:40:05 +0500

    Add OperandWrongFormatException

---
+++ WebCalc.Domain/Exceptions/OperandWrongFormatException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Exceptions
+{
+    public class OperandWrongFormatException : Exception
+    {
+        public OperandWrongFormatException(string message) 
+            : base(message) { }
+    }
+}
