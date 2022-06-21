98fdd2b1ce2a0ee5a8df2ac094524084e1d0d3b8
Bekzod Allaev <Allaev_@outlook.com>
2022-06-21 17:52:28 +0500

    Add constants for wrong operand exception messages

--- WebCalc.Domain/Constants/ExceptionMessageConstants.cs
+++ WebCalc.Domain/Constants/ExceptionMessageConstants.cs
@@ -11,5 +11,9 @@ namespace WebCalc.Domain.Constants
         public const string SET_FIRST_OPERAND_MESSAGE = "Please set first operand";
         public const string SET_SECOND_OPERAND_MESSAGE = "Please set second operand";
         public const string SET_OPERATOR_MESSAGE = "Please set operator first";
+
+        public const string FIRST_ZERO_MESSAGE = "First digit cannot be zero";
+        public const string TWO_FLOATING_POINT_MESSAGE = "Two floating point sign";
+        public const string TWO_MINUS_SIGN_MESSAGE = "Two minus sign";
     }
 }
--- WebCalc.Domain/Entities/Operand.cs
+++ WebCalc.Domain/Entities/Operand.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebCalc.Domain.Constants;
+using WebCalc.Domain.Exceptions;
 
 namespace WebCalc.Domain.Entities
 {
@@ -19,8 +21,13 @@ namespace WebCalc.Domain.Entities
 
         public void SetValue(string stringValue)
         {
-            if ((stringValue.Length > 1 && (stringValue.First() == '0' || stringValue.Substring(0, 2) == "--")) || stringValue.Where(x => x == ',').Count() > 1)
-                throw new Exception();
+            if (stringValue.Length > 1 && stringValue.First() == '0')
+                throw new OperandWrongFormatException(ExceptionMessageConstants.FIRST_ZERO_MESSAGE);
+            else if (stringValue.Length > 1 && stringValue.Substring(0, 2) == "--")
+                throw new OperandWrongFormatException(ExceptionMessageConstants.TWO_MINUS_SIGN_MESSAGE);
+            else if (stringValue.Where(x => x == ',').Count() > 1)
+                throw new OperandWrongFormatException(ExceptionMessageConstants.TWO_FLOATING_POINT_MESSAGE);
+
             Value = double.Parse(stringValue);
         }
 
