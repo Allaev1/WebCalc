08c9faec9297cf200f2ec81f0043c947a11dec3a
Bekzod Allaev <Allaev_@outlook.com>
2022-06-27 15:42:06 +0500

    Register resources

--- WebCalc/Program.cs
+++ WebCalc/Program.cs
@@ -1,11 +1,14 @@
 using WebCalc;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization(options => options.ResourcesPath = "WebCalc.Domain/Resources");
 
 await builder.Build().RunAsync();
