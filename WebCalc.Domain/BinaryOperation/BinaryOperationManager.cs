8493146023ff735fbec30bb98a09485b5d04f009
Bekzod Allaev <Allaev_@outlook.com>
2022-06-27 14:21:17 +0500

    Edit IBinaryOperationManager interface

--- WebCalc.Domain/BinaryOperation/BinaryOperationManager.cs
+++ WebCalc.Domain/BinaryOperation/BinaryOperationManager.cs
@@ -1,24 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Localization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebCalc.Domain.Entities;
+using WebCalc.Domain.BinaryOperation;
 
 namespace WebCalc.Domain.BinaryOperation
 {
     public class BinaryOperationManager : IBinaryOperationManager
     {
-        public BinaryOperation GetBinaryOperation() => new BinaryOperation();
+        public BinaryOperation BinaryOperation { get; }
 
-        public BinaryOperation GetChainedBinaryOperation(BinaryOperation operation)
+        public BinaryOperationManager(IStringLocalizer<BinaryOperationManager> stringLocalizer)
         {
-            var result = new BinaryOperation();
-
-            operation.CalculateResult();
-            result.FirstOperand.SetValue(operation.Result);
-
-            return operation;
+            BinaryOperation = new
+                (stringLocalizer["Operand1NotSetExceptionMessage"], 
+                stringLocalizer["OperationTypeNotSetExceptionMessage"], 
+                stringLocalizer["Operand2NotSetExceptionMessage"]);
         }
     }
 }
--- WebCalc.Domain/BinaryOperation/IBinaryOperationManager.cs
+++ WebCalc.Domain/BinaryOperation/IBinaryOperationManager.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebCalc.Domain.Entities;
 
 namespace WebCalc.Domain.BinaryOperation
 {
     public interface IBinaryOperationManager
     {
-        public BinaryOperation GetBinaryOperation();
-
-        public BinaryOperation GetChainedBinaryOperation(BinaryOperation operation);
+        public BinaryOperation BinaryOperation { get; }
     }
 }
