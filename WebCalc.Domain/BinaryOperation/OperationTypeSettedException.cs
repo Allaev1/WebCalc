696bd9ba079c98567129f91fc413e72e25c42627
Bekzod Allaev <Allaev_@outlook.com>
2022-06-27 15:01:19 +0500

    Add OperationTypeSettedException

---
+++ WebCalc.Domain/BinaryOperation/OperationTypeSettedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.BinaryOperation
+{
+    public class OperationTypeSettedException : Exception
+    {
+        public OperationTypeSettedException(string message)
+            : base(message) { }
+    }
+}
