abd416468299cee3909767d01c3fd0ae11805fc8
Bekzod Allaev <Allaev_@outlook.com>
2022-06-27 12:25:57 +0500

    Add OperationState enum

---
+++ WebCalc.Domain/BinaryOperation/OperationState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.BinaryOperation
+{
+    public enum OperationState
+    {
+        Operand1Set,
+        Operand2Set,
+        OperationTypeSet
+    }
+}
