773f5436e30e9ccc92812564f16c623ec557a987
Bekzod Allaev <Allaev_@outlook.com>
2022-06-28 23:36:10 +0500

    Delete OpeationState

--- WebCalc.Domain/BinaryOperation/BinaryOperation.cs
+++ WebCalc.Domain/BinaryOperation/BinaryOperation.cs
@@ -2,7 +2,6 @@
 {
     public class BinaryOperation
     {
-        private OperationState operationState;
         private readonly string operand1NotSetExceptionMessage;
         private readonly string operationTypeNotSetExceptionMessage;
         private readonly string operand2NotSetExceptionMessage;
@@ -16,7 +15,6 @@
             string operationTypeSettedExceptionMessage,
             string operand1SettedExceptionMessage)
         {
-            operationState = OperationState.Operand1NotSet;
             this.operand1NotSetExceptionMessage = operand1NotSetExceptionMessage;
             this.operationTypeNotSetExceptionMessage = operationTypeNotSetExceptionMessage;
             this.operand2NotSetExceptionMessage = operand2NotSetExceptionMessage;
@@ -24,46 +22,40 @@
             this.operand1SettedExceptionMessage = operand1SettedExceptionMessage;
         }
 
-        public float Operand1 { get; private set; }
+        public float? Operand1 { get; private set; }
 
-        public float Operand2 { get; private set; }
+        public float? Operand2 { get; private set; }
 
-        public OperationType OperationType { get; private set; }
+        public OperationType? OperationType { get; private set; }
 
-        public float Result { get; private set; }
+        public float? Result { get; private set; }
 
         public void SetOperand1(float value)
         {
-            if (operationState == OperationState.OperationTypeSet)
+            if(Operand1.HasValue)
                 throw new Operand1SettedException(operand1SettedExceptionMessage);
             Operand1 = value;
-            operationState = OperationState.Operand1Set;
-            Result = 0;
         }
 
         public void SetOperand2(float value)
         {
-            if (operationState != OperationState.OperationTypeSet)
+            if(!OperationType.HasValue)
                 throw new OperationTypeNotSetException(operationTypeNotSetExceptionMessage);
-
             Operand2 = value;
-            operationState = OperationState.Operand2Set;
         }
 
         public void SetOperationType(OperationType operationType)
         {
-            if (operationState != OperationState.Operand1Set)
+            if (!Operand1.HasValue)
                 throw new Operand1NotSetException(operand1NotSetExceptionMessage);
-            if (operationState == OperationState.Operand2Set)
+            if (Operand2.HasValue)
                 throw new OperationTypeSettedException(operationTypeSettedExceptionMessage);
-
             OperationType = operationType;
-            operationState = OperationState.OperationTypeSet;
         }
 
         public void CalculateResult()
         {
-            if (operationState != OperationState.Operand2Set)
+            if (!Operand2.HasValue)
                 throw new Operand2NotSetException(operand2NotSetExceptionMessage);
             Result = GetResult();
             StartNewOperation();
@@ -71,18 +63,17 @@
 
         private void StartNewOperation()
         {
-            Operand1 = 0;
-            Operand2 = 0;
-            OperationType = OperationType.NotSet;
-            operationState = OperationState.Operand1NotSet;
+            Operand1 = null;
+            Operand2 = null;
+            OperationType = null;
         }
 
-        private float GetResult() => OperationType switch
+        private float GetResult() => OperationType!.Value switch
         {
-            OperationType.Addition => Operand1 + Operand2,
-            OperationType.Subtraction => Operand1 - Operand2,
-            OperationType.Division => Operand1 / Operand2,
-            OperationType.Multiplication => Operand1 * Operand2,
+            Domain.BinaryOperation.OperationType.Addition => Operand1!.Value + Operand2!.Value,
+            Domain.BinaryOperation.OperationType.Subtraction => Operand1!.Value - Operand2!.Value,
+            Domain.BinaryOperation.OperationType.Division => Operand1!.Value / Operand2!.Value,
+            Domain.BinaryOperation.OperationType.Multiplication => Operand1!.Value * Operand2!.Value,
             _ => throw new NotImplementedException()
         };
     }
