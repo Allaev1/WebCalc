a3e6c71356717ccc4cb731086c85e33e462705f3
Bekzod Allaev <Allaev_@outlook.com>
2022-07-17 15:40:30 +0500

    Add IBinaryOperationAppService

---
+++ WebCalc.Application.Contracts/BinaryOperation/IBinaryOperationAppService.cs
@@ -0,0 +1,13 @@
+﻿using WebCalc.Domain.BinaryOperation;
+
+namespace WebCalc.Application.Contracts.BinaryOperation
+{
+    public interface IBinaryOperationAppService
+    {
+        public void SetOperand(string value);
+
+        public void SetOperationType(OperationType operationType);
+
+        public void SetResult();
+    }
+}
