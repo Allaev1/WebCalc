c8113e9994e00a586d74832346c37aa5f1b79e6c
Bekzod Allaev <Allaev_@outlook.com>
2022-07-23 16:15:30 +0500

    Refactor code

--- WebCalc.Domain/BinaryOperation/BinaryOperation.cs
+++ WebCalc.Domain/BinaryOperation/BinaryOperation.cs
@@ -63,7 +63,6 @@ namespace WebCalc.Domain.BinaryOperation
                 case OperationState.ResultSetted:
                     Operand1 = (float?)value;
                     Result = null;
-                    //OperationState = OperationState.Operand1Setted;
                     break;
             }
         }
