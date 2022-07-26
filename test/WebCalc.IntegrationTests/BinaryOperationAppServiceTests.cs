8b7865394338fd2e46a8c79750112e1faaf61265
Bekzod Allaev <Allaev_@outlook.com>
2022-07-27 01:02:23 +0500

    Edit tests for display and expression

--- test/WebCalc.IntegrationTests/BinaryOperationAppServiceTests.cs
+++ test/WebCalc.IntegrationTests/BinaryOperationAppServiceTests.cs
@@ -27,30 +27,46 @@ namespace WebCalc.IntegrationTests
 
         private void BinaryOperationAppService_ExpressionValueChanged(object? sender, string e)
         {
-            displayValue = e;
+            expressionValue = e;
         }
 
         private void BinaryOperationAppService_DisplayValueChanged(object? sender, string e)
         {
-            expressionValue = e;
+            displayValue = e;
         }
 
         [Theory]
         [InlineData(new char[] { '1', '2', '3' }, "123")]
         [InlineData(new char[] { '0', '0', '1', '2', '3' }, "123")]
-        [InlineData(new char[] { '1', ',', ',', '0' }, "120")]
+        [InlineData(new char[] { '1', ',', ',', '0' }, "1,0")]
         [InlineData(new char[] { ',', ',', '0' }, "0,0")]
+        [InlineData(new char[] { '0', ',', ',', '0', '1', '2', ',', '0' }, "0,0120")]
+        [InlineData(new char[] {'0','0','1','2',',',',','3','0',',','4','0'}, "12,3040")]
         public void TestDisplayValue(char[] values, string expected)
         {
             foreach (var value in values)
             {
-                if (char.IsDigit(value))
-                    binaryOperationAppService.EditDisplayValue(value);
-                else
-                    binaryOperationAppService.EditExpressionValue(value);
+                binaryOperationAppService.EditValues(value);
             }
 
             displayValue.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(new char[] { '1', '2', '3' }, "123")]
+        [InlineData(new char[] { '0', '0', '1', '2', '3' }, "123")]
+        [InlineData(new char[] { '1', ',', ',', '0' }, "1,0")]
+        [InlineData(new char[] { ',', ',', '0' }, "0,0")]
+        [InlineData(new char[] { '0', ',', ',', '0', '1', '2', ',', '0' }, "0,0120")]
+        [InlineData(new char[] { '0', '0', '1', '2', ',', ',', '3', '0', ',', '4', '0' }, "12,3040")]
+        public void TestExpressionValue(char[] values, string expected)
+        {
+            foreach (var value in values)
+            {
+                binaryOperationAppService.EditValues(value);
+            }
+
+            expressionValue.Should().Be(expected);
         }
     }
 }
