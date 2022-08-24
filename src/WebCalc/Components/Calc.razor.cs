bdb6a1d43e73c52ac73b4f5552e825ffe83d5d82
Bekzod Allaev <Allaev_@outlook.com>
2022-08-24 11:17:55 +0500

    Create cs file for razor file

---
+++ src/WebCalc/Components/Calc.razor.cs
@@ -0,0 +1,106 @@
+﻿using WebCalc.Domain.BinaryOperation;
+
+namespace WebCalc.Components
+{
+    public partial class Calc
+    {
+        public CalcDisplay? display;
+
+        private async Task UpdateDisplayAsync(char value)
+        {
+            if (display is null) throw new NullReferenceException();
+
+            if (value == Constants.MEMORY_ADD)
+            {
+                var memory = binaryOperationManager.GetMemoryAddResult(float.Parse(display!.Value));
+                display.SetMemory(memory.ToString());
+                return;
+            }
+            else if (value == Constants.MEMORY_CLEAR)
+            {
+                binaryOperationManager.ClearMemory();
+                display.ClearMemory();
+                return;
+            }
+            else if (
+                TryToBackspaceResult(value) ||
+                TryToBackspaceWhenOperand1Setted(value) ||
+                TryToExceedMaxCountOfCharsOnDisplay(value) ||
+                TryToNegateZero(value))
+                return;
+            else if (value == Constants.CLEAR)
+                binaryOperationManager.BinaryOperation.Clear();
+            else if (IsChainingCalculation(value))
+            {
+                binaryOperationManager.BinaryOperation.SetResult();
+                binaryOperationManager.BinaryOperation.SetOperand(binaryOperationManager.BinaryOperation.Result);
+                SetOperationType(value);
+
+                var operand1 = binaryOperationManager.BinaryOperation.Operand1.ToString()!;
+                var chars = new char[operand1.Count() + 1];
+                operand1.ToArray().CopyTo(chars, 0);
+                chars[operand1.Length] = value;
+
+                await display.AppendAsync(chars);
+            }
+            else
+            {
+                if (binaryOperationManager.BinaryOperation.OperationState is OperationState.ResultSetted && (char.IsDigit(value) || value == Constants.FLOATING_POINT))
+                {
+                    display.Clear();
+                    binaryOperationManager.BinaryOperation.Clear();
+                }
+
+                if (value == Constants.NEGATION_OPERATION_SIGN)
+                {
+                    binaryOperationManager.NegationOperation.SetOperand(float.Parse(display.Value));
+                    binaryOperationManager.NegationOperation.SetResult();
+                }
+            }
+
+            await display.AppendAsync(value);
+        }
+
+        private void SetOperand(float operand)
+        {
+            binaryOperationManager.BinaryOperation.SetOperand(operand);
+        }
+
+        private bool TryToBackspaceResult(char value)
+            => binaryOperationManager.BinaryOperation.OperationState is OperationState.ResultSetted && value == Constants.BACKSPACE;
+
+        private bool TryToExceedMaxCountOfCharsOnDisplay(char value)
+            => display!.Value.Count() == display.MaxDisplayCharsCount && (char.IsDigit(value) || value == Constants.FLOATING_POINT);
+
+        private bool TryToBackspaceWhenOperand1Setted(char value)
+            => value == Constants.BACKSPACE && binaryOperationManager.BinaryOperation.OperationState is OperationState.Operand1Setted;
+
+        private bool TryToNegateZero(char value)
+            => value == Constants.NEGATION_OPERATION_SIGN && display!.Value == "0";
+
+        private bool IsChainingCalculation(char value) =>
+            (value == '+' || value == '-' || value == '*' || value == '/') &&
+            binaryOperationManager.BinaryOperation.Operand2 is not null;
+
+        private void SetOperationType(char value)
+        {
+            switch (value)
+            {
+                case '+':
+                    binaryOperationManager.BinaryOperation.SetOperationType(OperationType.Addition);
+                    break;
+                case '-':
+                    binaryOperationManager.BinaryOperation.SetOperationType(OperationType.Subtraction);
+                    break;
+                case '*':
+                    binaryOperationManager.BinaryOperation.SetOperationType(OperationType.Multiplication);
+                    break;
+                case '/':
+                    binaryOperationManager.BinaryOperation.SetOperationType(OperationType.Division);
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ src/WebCalc/Components/CalcDisplay.razor.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebCalc.Domain.BinaryOperation;
+
+namespace WebCalc.Components
+{
+    public partial class CalcDisplay
+    {
+        private const string INITIAL_STRING = "0";
+        private string value = INITIAL_STRING;
+        private string expression = string.Empty;
+        private string memory = string.Empty;
+        private bool showMemory;
+
+        [Parameter]
+        public int MaxDisplayCharsCount { get; set; } = 15;
+
+        [Parameter]
+        public int MaxFractionalDigitsCount { get; set; } = 9;
+
+        [Parameter]
+        public EventCallback<float> OnValidOperandGenerated { get; set; }
+
+        public void SetMemory(string memory)
+        {
+            this.memory = memory;
+            showMemory = true;
+        }
+
+        public void ClearMemory()
+        {
+            this.memory = string.Empty;
+            showMemory = false;
+        }
+
+        public string Value => value;
+
+        public string Expression => expression;
+
+        public string Memory => memory;
+
+        public void Clear()
+        {
+            value = INITIAL_STRING;
+            expression = string.Empty;
+        }
+
+        public async Task AppendAsync(char @char)
+        {
+            var oldValue = value;
+            value = GetValidOperand(value, @char);
+
+            if (oldValue != value)
+            {
+                await OnValidOperandGenerated.InvokeAsync(float.Parse(value));
+            }
+
+            expression = GetValidExpression(expression, @char);
+
+            StateHasChanged();
+        }
+
+        public async Task AppendAsync(char[] chars)
+        {
+            foreach (var @char in chars)
+            {
+                await AppendAsync(@char);
+            }
+        }
+
+        private string GetValidExpression(string expression, char @char)
+        {
+            (string? firstOperand, char? operationType, string? secondOperand) = GetExpressionComponents(expression);
+
+            if (secondOperand is not null)
+            {
+                secondOperand = GetValidOperand(secondOperand, @char);
+            }
+            else if (operationType is not null)
+            {
+                if (char.IsDigit(@char))
+                {
+                    secondOperand = GetValidOperand(string.Empty, @char);
+                }
+                else
+                {
+                    operationType = @char;
+                }
+            }
+            else if (firstOperand is not null)
+            {
+                firstOperand = GetValidOperand(firstOperand, @char);
+            }
+
+            return string.Concat(firstOperand, operationType, secondOperand);
+        }
+
+        private (string?, char?, string?) GetExpressionComponents(string expression)
+        {
+            var operands = expression.Split('+', '-', '*', '/');
+            var operationTypeIndex = expression.IndexOfAny(new[] { '+', '-', '*', '/' });
+
+            if (operands.ElementAtOrDefault(1) is string secondOperand)
+            {
+                return (operands[0], expression[operationTypeIndex], secondOperand);
+            }
+            else if (operationTypeIndex > 0)
+            {
+                return (operands[0], expression[operationTypeIndex], null);
+            }
+            else
+            {
+                return (operands[0], null, null);
+            }
+        }
+
+        private string GetValidOperand(string source, char @char)
+        {
+            string temp = string.Empty;
+
+            if (source == INITIAL_STRING && @char != Constants.FLOATING_POINT)
+                temp += @char;
+            else
+                temp = source + @char;
+
+            if (float.TryParse(temp, out float res))
+                return temp;
+            else
+                return source;
+        }
+    }
+}
