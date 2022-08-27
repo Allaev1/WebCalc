5a5924b149d460944cde0e49c6ecfdeabc7b0546
Bekzod Allaev <Allaev_@outlook.com>
2022-08-28 01:14:09 +0500

    Empty IBinaryOperationAppService

--- src/WebCalc.Application.Contracts/BinaryOperation/IBinaryOperationAppService.cs
+++ src/WebCalc.Application.Contracts/BinaryOperation/IBinaryOperationAppService.cs
@@ -4,16 +4,5 @@ namespace WebCalc.Application.Contracts.BinaryOperation
 {
     public interface IBinaryOperationAppService
     {
-        public event EventHandler<string> DisplayValueChanged;
-
-        public event EventHandler<string> ExpressionValueChanged;
-
-        public event EventHandler<string> MemoryValueChanged;
-
-        /// <summary>
-        /// Edit display and expression values
-        /// </summary>
-        /// <param name="value"></param>
-        public void EditValues(char value);
     }
 }
