2cb3fb0d7ebb2b80f9c4f7a338791528b1dfa374
Bekzod Allaev <Allaev_@outlook.com>
2022-09-05 01:10:41 +0500

    Add MemoryColor, FigureColor, PercentageOffColor, OperationTypeColor, ConstColor, BackspaceAndClearColor, and ButtonForgeGroundColor parameters to ICalcKeyboard. Simplify html

--- src/WebCalc/Contracts/ICalcKeyboard.cs
+++ src/WebCalc/Contracts/ICalcKeyboard.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Drawing;
 
 namespace WebCalc.Contracts
 {
@@ -6,5 +7,26 @@ namespace WebCalc.Contracts
     {
         [Parameter]
         public EventCallback<char> OnButtonClick { get; set; }
+
+        [Parameter]
+        public Color MemoryColor { get; set; }
+
+        [Parameter]
+        public Color FigureColor { get; set; }
+
+        [Parameter]
+        public Color ConstColor { get; set; }
+
+        [Parameter] 
+        public Color OperationTypeColor { get; set; }
+
+        [Parameter]
+        public Color BackspaceAndClearColor { get; set; }
+
+        [Parameter]
+        public Color PercentageOffColor { get; set; }
+
+        [Parameter]
+        public Color ButtonForgeGroundColor { get; set; }
     }
 }
