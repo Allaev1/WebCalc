6f2f557ff4142e7d29d2d2cd045b52785db6785e
Bekzod Allaev <Allaev_@outlook.com>
2022-09-28 21:33:49 +0500

    Add INavigationHistory

---
+++ src/WebCalc/Services/INavigationHistory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WebCalc.Services
+{
+    public interface INavigationHistory
+    {
+        public ReadOnlyCollection<string> History { get; }
+    }
+}
