dd2c2218106892f81b2a8ccc82719260253dfc3e
Bekzod Allaev <Allaev_@outlook.com>
2022-09-29 18:17:55 +0500

    Edit IBackNavigateable contract and implement it

---
+++ src/WebCalc/Services/NavigationHistoryStorage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WebCalc.Services
+{
+    public class NavigationHistoryStorage : IBackNavigateable
+    {
+        private Stack<string> locationsStack = new();
+
+        public string GetNaivgateBackLocation()
+        {
+            var location = locationsStack.Pop();
+            return location;
+        }
+
+        public void AddCurrentLocation(string location)
+        {
+            locationsStack.Push(location);
+        }
+    }
+}
