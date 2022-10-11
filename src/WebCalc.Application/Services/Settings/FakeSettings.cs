734191815035dd2665ae6b349d0a930224802c17
Bekzod Allaev <Allaev_@outlook.com>
2022-10-11 23:38:17 +0500

    Add settings service and its fake realization

---
+++ src/WebCalc.Application/Services/Settings/FakeSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCalc.Application.Contracts.Services.Settings;
+
+namespace WebCalc.Application.Services.Settings
+{
+    public class FakeSettings : ISettings
+    {
+        public int MaxDisplayCharsCount { get; set; } = 15;
+    }
+}
