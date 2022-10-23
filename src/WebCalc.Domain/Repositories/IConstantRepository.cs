cd6d6dbbf7e60a1163ecca27e36ac1b0b067eee5
Bekzod Allaev <Allaev_@outlook.com>
2022-10-24 01:01:54 +0500

    Add IConstantRepository

---
+++ src/WebCalc.Domain/Repositories/IConstantRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Domain.Repositories
+{
+    public interface IConstantRepository
+    {
+        public Task<IEnumerable<Constant.Constant>> GetAllAsync();
+
+        public Task<Constant.Constant> GetByIdAsync(Guid id);
+
+        public Task<Constant.Constant> CreateAsync(string name, float value, string? description = null);
+
+        public Task DeleteAsync(Guid id);
+
+        public Task<Constant.Constant> UpdateAsync(Guid id, Constant.Constant value);
+    }
+}
