698124b06926bc96d58e38af8f40c072074ba41a
Bekzod Allaev <Allaev_@outlook.com>
2022-10-27 23:21:29 +0500

    Add DTOs for constant

---
+++ src/WebCalc.Application.Contracts/Constants/DTO/ConstantDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Application.Contracts.Constants.DTO
+{
+    public class ConstantDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public float Value { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ src/WebCalc.Application.Contracts/Constants/DTO/UpdateConstantDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalc.Application.Contracts.Constants.DTO
+{
+    public class UpdateConstantDto
+    {
+        public string Name { get; set; } = null!;
+
+        public float Value { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
