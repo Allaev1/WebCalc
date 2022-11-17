3cb702b63fb67dc9c372d57be46cf81cbe0bb047
Bekzod Allaev <Allaev_@outlook.com>
2022-11-18 00:33:59 +0500

    Changes related to AddEditConst form

---
+++ src/WebCalc.Blazor/Validation/AddEditConstValidationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCalc.Blazor.Validation
+{
+    public class AddEditConstValidationModel
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(typeof(float), "1", "1000000", ErrorMessage = $"Value should be in range 1-1 000 000")]
+        public float? Value { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
