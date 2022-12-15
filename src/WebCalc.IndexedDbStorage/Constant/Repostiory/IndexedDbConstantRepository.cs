c012a43706380cbd6f81fedfdda4c9967e4f4192
Bekzod Allaev <Allaev_@outlook.com>
2022-12-15 18:43:46 +0500

    Calculate object store name and use overloading with object store name

--- src/WebCalc.IndexedDbStorage/Constant/Repostiory/IndexedDbConstantRepository.cs
+++ src/WebCalc.IndexedDbStorage/Constant/Repostiory/IndexedDbConstantRepository.cs
@@ -6,35 +6,39 @@ namespace WebCalc.IndexedDbStorage.Constant.Repostiory
     public class IndexedDbRepository<T> : IRepository<T>
     {
         private readonly WebCalcDb webCalcDb;
+        private readonly string objectStoreName;
+        private const char PluralEnding = 's';
 
         public IndexedDbRepository(WebCalcDb webCalcDb)
         {
             this.webCalcDb = webCalcDb;
+            objectStoreName = $"{typeof(T).Name.Replace("Proxy", string.Empty)}{PluralEnding}".ToLower();
         }
 
         public async Task CreateAsync(T entity)
         {
-            await webCalcDb.AddItems(new List<T> { entity });
+            await webCalcDb.AddItems(objectStoreName, new List<T> { entity });
         }
 
         public async Task DeleteAsync(Guid id)
         {
-            await webCalcDb.DeleteByKey<Guid, T>(id);
+            await webCalcDb.DeleteByKey<Guid>(objectStoreName, id);
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await webCalcDb.GetAll<T>();
+            await webCalcDb.OpenIndexedDb();
+            return await webCalcDb.GetAll<T>(objectStoreName);
         }
 
         public async Task<T> GetByIdAsync(Guid id)
         {
-            return await webCalcDb.GetByKey<Guid, T>(id);
+            return await webCalcDb.GetByKey<Guid, T>(objectStoreName, id);
         }
 
         public async Task UpdateAsync(Guid id, T entity)
         {
-            await webCalcDb.UpdateItems(new List<T> { entity });
+            await webCalcDb.UpdateItems(objectStoreName, new List<T> { entity });
         }
     }
 }
