9cb5dee76469e07502c8b04e844c1af6da4939e0
Bekzod Allaev <Allaev_@outlook.com>
2022-12-28 21:34:22 +0500

    Add service for storing user options

---
+++ src/WebCalc.Blazor/Options/ICustomOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace WebCalc.Blazor.Options
+{
+    public interface ICustomOptions
+    {
+        public Task CreateAsync<T>(string name, T value);
+
+        public Task DeleteAsync(string name);
+
+        public Task UpdateAsync<T>(string name, T value);
+
+        public Task<T> GetAsync<T>(string name);
+    }
+}
